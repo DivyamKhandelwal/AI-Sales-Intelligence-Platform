02e2bd9e889e07f0a7580bf7daba7c2e68609ae6
DivyamKhandelwal <divyamkhandelwal010@gmail.com>
2026-05-21 14:23:28 +0530

    Added frontend application properly

---
+++ frontend/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white flex flex-col items-center justify-center">
+
+      <h1 className="text-6xl font-bold mb-6">
+        AI Sales Intelligence
+      </h1>
+
+      <p className="text-gray-400 text-xl mb-8 text-center max-w-2xl">
+        Upload cold calls and analyze customer sentiment,
+        buying intent, objections, and conversion probability using AI.
+      </p>
+
+      <button className="bg-blue-600 hover:bg-blue-700 px-8 py-4 rounded-2xl text-lg font-semibold transition">
+        Upload Audio
+      </button>
+
+    </main>
+  );
+}
+<p className="text-red-500">Testing Git Tracking</p>
