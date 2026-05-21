417c46a7098ef35123530ab21ee0d07d9f53a473
DivyamKhandelwal <divyamkhandelwal010@gmail.com>
2026-05-21 21:05:01 +0530

    Connected frontend with backend upload API

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import shutil
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Backend Running Successfully"
+    }
+
+@app.post("/upload-audio")
+async def upload_audio(file: UploadFile = File(...)):
+
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "filename": file.filename,
+        "message": "Audio uploaded successfully"
+    }
--- frontend/components/UploadCard.tsx
+++ frontend/components/UploadCard.tsx
@@ -1,4 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+
 export default function UploadCard() {
+
+  const [file, setFile] = useState<File | null>(null);
+  const [message, setMessage] = useState("");
+
+  const handleUpload = async () => {
+
+    if (!file) {
+      alert("Please select a file");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+
+      const response = await axios.post(
+        "http://127.0.0.1:8000/upload-audio",
+        formData
+      );
+
+      console.log(response.data);
+setMessage("Audio uploaded successfully");
+
+    } catch (error) {
+      console.error(error);
+      setMessage("Upload failed");
+
+    }
+  };
+
   return (
     <div className="bg-slate-900 border border-slate-800 p-8 rounded-3xl shadow-2xl w-[500px]">
 
@@ -19,12 +55,26 @@ export default function UploadCard() {
 
       <input
         type="file"
+        onChange={(e) => {
+          if (e.target.files) {
+            setFile(e.target.files[0]);
+          }
+        }}
         className="w-full bg-slate-800 p-3 rounded-xl mb-6"
       />
 
-      <button className="w-full bg-blue-600 hover:bg-blue-700 transition p-4 rounded-xl font-semibold">
+      <button
+        onClick={handleUpload}
+        className="w-full bg-blue-600 hover:bg-blue-700 transition p-4 rounded-xl font-semibold"
+      >
         Analyze Call
       </button>
+
+      {message && (
+        <p className="mt-4 text-green-400">
+          {message}
+        </p>
+      )}
 
     </div>
   );
